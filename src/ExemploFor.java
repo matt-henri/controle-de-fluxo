6d4f22085572ae1225ad9b7a7b48b1a62711ffc2
matt-henrii <mathmarkub@gmai.com>
2023-09-03 16:13:45 -0300

    estudo de for

---
+++ src/ExemploFor.java
@@ -0,0 +1,8 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for (int carneirinhos = 1; carneirinhos <= 15; carneirinhos++) {
+            System.out.println("Contando Carneirinhos " + carneirinhos);
+        }
+        System.out.println("Pedro dormiu...zzz");
+    }
+}
