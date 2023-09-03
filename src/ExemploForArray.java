b91b5fcc986d9362dacd4ff4f3c3d61526ba34cf
matt-henrii <mathmarkub@gmai.com>
2023-09-03 16:23:58 -0300

    for array atualizado

--- src/ExemploForArray.java
+++ src/ExemploForArray.java
@@ -2,8 +2,12 @@ public class ExemploForArray {
     public static void main(String[] args) {
         String alunos[] = { "Felipe", "Jonas", "Julia", "Marcos" };
 
-        for (int x = 0; x < alunos.length; x++) {
-            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        // for (int x = 0; x < alunos.length; x++) {
+        // System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        // }
+
+        for (String aluno : alunos) {
+            System.out.println("Nome do aluno é: " + aluno);
         }
     }
 }
