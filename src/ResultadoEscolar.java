237a5db1d2a4c3bfc143a865d992d22e9f8c456c
matt-henrii <mathmarkub@gmai.com>
2023-09-03 15:33:49 -0300

    estudo if else

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,9 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 8;
 
         if (nota >= 7)
             System.out.println("Aprovado!");
+
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova de Recuperação.");
 
         else
             System.out.println("Reprovado!");
